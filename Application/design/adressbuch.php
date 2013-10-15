2de7c462b094e9b333ce39f0dacdd68564a15898
Julia Menzel <jumenz@web.de>
2013-10-15 21:52:31 +0200

    adressbuch edit formular verschönern

--- Application/design/adressbuch.php
+++ Application/design/adressbuch.php
@@ -7,14 +7,16 @@ include_once('head.html');
 <!-- page specific settings -->
 <title>Este Frauen | Adressbuch</title>
 <meta name="robots" content="noindex, nofollow">
+<link rel="stylesheet" type="text/css" href="../../css/addresses.css"/>
 <script type="text/javascript" src="../../js/db.js"></script>
 </head>
 <?php
 $isAdmin = false; // TODO test auf admin -> hier ändern für andere testansicht!
-$userId = ''; // TODO userid eintragen
+$userId = '2'; // TODO userid eintragen
 // $dataArray = $model->getListData(); // aufruf für db anbindung
 $dataArray = array(
     '0' => array(
+        'id' => '1',
         'name' => 'Menzel',
         'prename' => 'Julia',
         'date_birth' => '22.12.1984',
@@ -27,6 +29,7 @@ $dataArray = array(
         'phone' => '040-1234567'
     ),
     '1' => array(
+        'id' => '2',
         'name' => 'Schwartau',
         'prename' => 'Ellen',
         'date_birth' => '11.06.1992',
@@ -44,7 +47,7 @@ $rows = count($dataArray);
 //print_r($dataArray);
 ?>
 </head>
-<body>
+<body id="addresses">
 <!-- header -->
 <?php include_once('header.html'); ?>
 <!-- content der Seite -->
@@ -133,74 +136,83 @@ $rows = count($dataArray);
                                 <?php if ($dataArray[$i]['id'] == $userId || $isAdmin) : ?>
                                 <div class="box-body toggle-content box-borders-bottom" style="display: none;">
                                     <form name="edit" action="../Controller/AddressesController.php" method="post" >
-                                        <fieldset id="contact">
-                                            <div class="form-item required clearfix">
-                                                <label for="name">Name</label>
-                                                <p class="input">
-                                                    <input type="text" id="name" class="input input-text required-entry" title="Name"
-                                                           name="name" maxlength="255" value="<?php echo $dataArray[$i]['name']; ?>">
-                                                </p>
-                                            </div>
-                                            <div class="form-item required clearfix">
-                                                <label for="prename">Vorname</label>
-                                                <p class="input">
-                                                    <input type="text" id="prename" class="input input-text required-entry" title="Vorname"
-                                                           name="prename" maxlength="255" value="<?php echo $dataArray[$i]['prename']; ?>">
-                                                </p>
-                                            </div>
-                                            <div class="form-item clearfix">
-                                                <label for="date-birth">Geburtsdatum</label>
-                                                <p class="input">
-                                                    <input type="text" id="date-birth" class="input input-text" title="Geburtsdatum"
-                                                           name="date_birth" maxlength="10" value="<?php echo $dataArray[$i]['date_birth']; ?>">
-                                                </p>
-                                            </div>
-                                            <div class="form-item clearfix">
-                                                <label for="street">Strasse, Nr</label>
-                                                <p class="input">
-                                                    <input type="text" id="street" class="input input-text left-text" title="Strasse" name="street"
-                                                           maxlength="255" value="<?php echo $dataArray[$i]['street']; ?>">
-                                                    <input type="text" id="nr" class="input input-text right-text" title="Hausnummer" name="nr"
-                                                           maxlength="6" value="<?php if ($dataArray[$i]['nr'] != '0') { echo $dataArray[$i]['nr']; } ?>">
-                                                </p>
-                                            </div>
-                                            <div class="form-item clearfix">
-                                                <label for="city">PLZ, Ort</label>
-                                                <p class="input">
-                                                    <input type="text" id="zipcode" class="input input-text left-text" title="Postleitzahl" name="zipcode"
-                                                           maxlength="5" value="<?php echo $dataArray[$i]['zipcode']; ?>">
-                                                    <input type="text" id="city" class="input input-text right-text" title="Stadt" name="city"
-                                                           maxlength="255" value="<?php echo $dataArray[$i]['city']; ?>">
-                                                </p>
-                                            </div>
-                                            <div class="form-item required clearfix">
-                                                <label for="mail">E-Mail</label>
-                                                <p class="input">
-                                                    <input type="text" id="mail" class="input input-text required-entry" title="E-Mail-Adresse"
-                                                           name="mail" maxlength="255" value="<?php echo $dataArray[$i]['mail']; ?>">
-                                                </p>
-                                            </div>
-                                            <div class="form-item">
-                                                <label for="phone">Festnetznummer (040-123456)</label>
-                                                <p class="input">
-                                                    <input type="text" id="phone" class="input input-text" title="Festnetznummer" name="phone"
-                                                           maxlength="32" value="<?php echo $dataArray[$i]['phone']; ?>">
-                                                </p>
-                                            </div>
-                                            <div class="form-item">
-                                                <label for="phone">Handynummer (0160-123456)</label>
-                                                <p class="input">
-                                                    <input type="text" id="mobile" class="input input-text" title="Handynummer" name="mobile"
-                                                           maxlength="32" value="<?php echo $dataArray[$i]['mobile']; ?>">
-                                                </p>
-                                            </div>
-                                        </fieldset>
+                                        <ul>
+                                            <li class="first two-col">
+                                                <fieldset class="first">
+                                                    <div class="form-item">
+                                                        <label for="prename">Vorname *</label>
+                                                        <p class="input">
+                                                            <input type="text" id="prename" class="input input-text required-entry" title="Vorname"
+                                                                   name="prename" maxlength="255" value="<?php echo $dataArray[$i]['prename']; ?>">
+                                                        </p>
+                                                    </div>
+                                                    <div class="form-item">
+                                                        <label for="name">Name *</label>
+                                                        <p class="input">
+                                                            <input type="text" id="name" class="input input-text required-entry" title="Name"
+                                                                   name="name" maxlength="255" value="<?php echo $dataArray[$i]['name']; ?>">
+                                                        </p>
+                                                    </div>
+                                                    <div class="form-item">
+                                                        <label for="street">Strasse, Nr</label>
+                                                        <p class="input">
+                                                            <input type="text" id="street" class="input input-text left-text" title="Strasse" name="street"
+                                                                   maxlength="255" value="<?php echo $dataArray[$i]['street']; ?>">
+                                                            <input type="text" id="nr" class="input input-text right-text" title="Hausnummer" name="nr"
+                                                                   maxlength="6" value="<?php if ($dataArray[$i]['nr'] != '0') { echo $dataArray[$i]['nr']; } ?>">
+                                                        </p>
+                                                    </div>
+                                                    <div class="form-item">
+                                                        <label for="city">PLZ, Ort</label>
+                                                        <p class="input">
+                                                            <input type="text" id="zipcode" class="input input-text left-text" title="Postleitzahl" name="zipcode"
+                                                                   maxlength="5" value="<?php echo $dataArray[$i]['zipcode']; ?>">
+                                                            <input type="text" id="city" class="input input-text right-text" title="Stadt" name="city"
+                                                                   maxlength="255" value="<?php echo $dataArray[$i]['city']; ?>">
+                                                        </p>
+                                                    </div>
+                                                </fieldset>
+                                            </li>
+                                            <li class="last two-col">
+                                                <fieldset class="last clearfix">
+                                                    <div class="form-item required">
+                                                        <label for="mail">E-Mail *</label>
+                                                        <p class="input">
+                                                            <input type="text" id="mail" class="input input-text required-entry" title="E-Mail-Adresse"
+                                                                   name="mail" maxlength="255" value="<?php echo $dataArray[$i]['mail']; ?>">
+                                                        </p>
+                                                    </div>
+                                                    <div class="form-item">
+                                                        <label for="phone">Festnetznummer (040-123456)</label>
+                                                        <p class="input">
+                                                            <input type="text" id="phone" class="input input-text" title="Festnetznummer" name="phone"
+                                                                   maxlength="32" value="<?php echo $dataArray[$i]['phone']; ?>">
+                                                        </p>
+                                                    </div>
+                                                    <div class="form-item">
+                                                        <label for="phone">Handynummer (0160-123456)</label>
+                                                        <p class="input">
+                                                            <input type="text" id="mobile" class="input input-text" title="Handynummer" name="mobile"
+                                                                   maxlength="32" value="<?php echo $dataArray[$i]['mobile']; ?>">
+                                                        </p>
+                                                    </div>
+                                                    <div class="form-item">
+                                                        <label for="date-birth">Geburtsdatum</label>
+                                                        <p class="input">
+                                                            <input type="text" id="date-birth" class="input input-text" title="Geburtsdatum"
+                                                                   name="date_birth" maxlength="10" value="<?php echo $dataArray[$i]['date_birth']; ?>">
+                                                        </p>
+                                                    </div>
+                                                </fieldset>
+                                            </li>
+                                        </ul>
+                                        <hr />
                                         <fieldset id="buttons">
-                                            <div class="form-submit clearfix">
+                                            <div class="form-submit">
+                                                <p id="required">* Pflichtfelder</p>
                                                 <p>
-                                                    <input name="cancel" type="button" value="Abbrechen " onclick="window.location.href = 'adressbuch.php';" >
-                                                    <input name="reset" type="reset" value="Zurücksetzen">
-                                                    <button type="submit" name="action" value="update">Aktualisieren</button>
+                                                    <button class="dark-bg" type="reset" name="reset" value="update"><div class="forward-raquo menu-link right"></div>Zurücksetzen</button>
+                                                    <button class="dark-bg" type="submit" name="action" value="update"><div class="forward-raquo menu-link right"></div>Aktualisieren</button>
                                                 </p>
                                             </div>
                                         </fieldset>
@@ -246,6 +258,13 @@ $rows = count($dataArray);
             $(itm).find('.toggle').on('click', function() {
                 $(itm).find('.toggle-content').toggle(200);
             });
+        });
+    });
+</script>
+<script type="text/javascript">
+    $( function() {
+        $("button[data-href]").click( function() {
+            location.href = $(this).attr("data-href");
         });
     });
 </script>
